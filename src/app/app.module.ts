f677a577d7ab0e89f35a55985861ee83eb782f9c
Kristopher Fana <kfana@africau.edu>
2023-06-05 15:36:18 +0200

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+
   ],
   imports: [
     BrowserModule,
